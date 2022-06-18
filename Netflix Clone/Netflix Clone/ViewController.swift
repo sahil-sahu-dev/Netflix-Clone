a672ccff6084da63eb289940121c4252c997eefc
sahil-sahu-dev <80636783+sahil-sahu-dev@users.noreply.github.com>
2022-06-18 21:28:25 +0530

    Add files via upload

---
+++ Netflix Clone/Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Sahil Sahu on 18/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
